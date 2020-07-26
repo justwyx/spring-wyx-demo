86e7106aae959c940a352952ebd381b5bf9cd1fe
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-07-26 22:20:26 +0800

    add

---
+++ myspring1/src/main/java/com/wyx/UserService.java
@@ -0,0 +1,23 @@
+package com.wyx;
+
+/**
+ * @author : Just wyx
+ * @Description : TODO 2020/7/26
+ * @Date : 2020/7/26
+ */
+public class UserService {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void sayHello() {
+		System.out.println("hello world");
+	}
+
+}
---
+++ myspring1/src/test/java/com/wyx/Test.java
@@ -0,0 +1,13 @@
+package com.wyx;
+
+/**
+ * @author : Just wyx
+ * @Description : TODO 2020/7/26
+ * @Date : 2020/7/26
+ */
+public class Test {
+	public static void main(String[] args) {
+		// 使用传统的方法，来调用userService的 sayHello方法
+		new UserService().sayHello();
+	}
+}
